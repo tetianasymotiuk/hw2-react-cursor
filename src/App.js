a582a4cb3e2eccb22c208ea2bdadf713af7ca37a
Tetiana <tetianaprivat@gmail.com>
2020-04-02 22:12:11 +0300

    hw 02 react - done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
 import './App.css';
+
+import Contacts from "./components/Contacts/Contacts";
+
 
 function App() {
   return (
     <div className="App">
-      <p>hw 02 react</p>
+       <Contacts />
     </div>
   );
 }
